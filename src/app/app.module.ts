d8dbd2b29c3f2e91d1fa385c73e3f67681e761f8
Tyler Kanz <tylerkanz@gmail.com>
2022-11-17 14:38:29 -0500

    initial navigation, home page, and animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { NgIconsModule } from '@ng-icons/core';
+import { typSocialGithub, typSocialLinkedin } from '@ng-icons/typicons'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgIconsModule.withIcons({ typSocialGithub,typSocialLinkedin }),
   ],
   providers: [],
   bootstrap: [AppComponent]
